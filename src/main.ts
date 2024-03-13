0193d8cd6415359ad233b4b72934d20f0aaab9fa
Alex Suzuki <alex@pixelverse.ch>
2024-03-13 15:56:49 +0100

    Ensure SDK is initialized when navigating to a route that requires barcode scanning

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './assets/main.css'
-import { StrichSDK } from "@pixelverse/strichjs-sdk";
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,6 +7,3 @@ import router from './router'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-const licenseKey = '<your license key>';
-StrichSDK.initialize(licenseKey);
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,27 +4,51 @@ import HomeView from '@/views/HomeView.vue'
 import ScanSingle from "@/views/ScanSingle.vue";
 import ScanRepeated from "@/views/ScanRepeated.vue";
 import ScanMultiple from "@/views/ScanMultiple.vue";
+import { StrichSDK } from "@pixelverse/strichjs-sdk";
+
+/**
+ * An example navigation guard that blocks navigation to a route that requires barcode scanning.
+ * This approach performs SDK initialization lazily, i.e. only when actually needed.
+ *
+ * It would also be possible to perform the SDK initialization in the Scanner component, this is just one possible way.
+ */
+async function ensureStrichSDKInitialized() {
+    if (!StrichSDK.isInitialized()) {
+        const licenseKey = '<your license key>';
+        try {
+            await StrichSDK.initialize(licenseKey);
+            return true;
+        } catch (e) {
+            return '/'; // navigate home, you probably want to handle this differently
+        }
+    }
+    return true;
+}
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            component: HomeView
+            component: HomeView,
         },
         {
             path: '/scan-single',
-            component: ScanSingle
+            component: ScanSingle,
+            beforeEnter: [ensureStrichSDKInitialized]
         },
         {
             path: '/scan-repeated',
-            component: ScanRepeated
+            component: ScanRepeated,
+            beforeEnter: [ensureStrichSDKInitialized]
         },
         {
             path: '/scan-multiple',
-            component: ScanMultiple
+            component: ScanMultiple,
+            beforeEnter: [ensureStrichSDKInitialized]
         }
     ]
-})
+});
+
 
 export default router
